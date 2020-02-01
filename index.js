d88a5a3b2450ba0e9cd227a9889f8a1e5a6d2a74
AnaClarah <58152037+AnaClarah@users.noreply.github.com>
2020-02-01 00:10:53 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+
+client.on('ready', () => {
+	console.log('Logado')
+	checkTimers();
+});
+function checkTimers() {
+	setInterval(function() {
+		let data = db.get('users').value()
+		console.log(data)
+		data.forEach(element => {
+			console.log(element)
+			if(element.time+2592000000 < Date.now()) {
+				client.users.get(element.id).send('o que vai ser avisado em 30 dias');
+				db.get('users').remove({id: element.id}).write();
+
+			}
+		})
+	}, 15000)
+}
+
+client.on('message', msg => {
+	if(msg.content.startsWith('!comando-setar')) {
+		let data = db.get('users')
+			.find({ id: msg.author.id })
+			.value();
+		if(typeof data == 'undefined'){
+			db.get('users')
+			.push({ id: msg.author.id, time: Date.now()})
+			.write()		
+		}
+		msg.reply('vou falar algo em 30 dias');
+	}
+	
+});
+
+client.login('NjcyOTk1MzYwNDMxNTM4MjA3.XjTl0w.Bp214OAgCBJZg20xaa97OlzFW98');
